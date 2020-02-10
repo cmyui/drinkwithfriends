bca1646b6674e30cd0b1d83683fc702f86bf6bd1
cmyui <cmyuisuki@gmail.com>
2020-02-10 10:25:21 -0500

    HOW

--- common/helpers/packetHelper.py
+++ common/helpers/packetHelper.py
@@ -63,6 +63,7 @@ class Packet(object):
     def read_data(self, data) -> None:
         self.data = bytearray(data.encode())
         self.id, self.length = _unpack('<hi', self.data)
+        self.offset += calcsize('<hi')
         return
 
     @staticmethod
