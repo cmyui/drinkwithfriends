15debefb54e6a141da3100208991e720aa1074bf
cmyui <cmyuisuki@gmail.com>
2020-02-10 19:39:49 -0500

    fdg

--- server/objects/glob.py
+++ server/objects/glob.py
@@ -6,7 +6,13 @@ from common.objects.user import User
 
 start_time: float = time()
 
-users: List[User] = [1, 5, 3, 7]
+users: List[User] = [
+    User(51, 'senor', 1, 100),
+    User(432, 'ds', 0, 100),
+    User(12, 'hth', 8, 100),
+    User(5, 'yes', 1, 100),
+    User(32, 'yeahahe', 1, 100)
+]
 
 db: Optional[MySQLConnectionPool] = None
 config: Optional[Any] = None
